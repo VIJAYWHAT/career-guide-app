c7e07881a271b7e43a0476c12ed3bcade8ec7e20
VijayWhat <vijayrv042@gmail.com>
2026-02-03 16:45:05 +0530

    deploy file changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/career-guide-app/",
+  base: "/career-guide-app",
   plugins: [react()],
   optimizeDeps: {
     exclude: ["lucide-react"],
